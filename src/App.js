0423fb481d76fbcbd5b1541d0177f531ebd04f43
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-10-30 12:27:40 -0300

    armado de nv para phone

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Header from './components/headerr/header';
 import './App.css';
+import NavegacionBar from './components/nav/navbar';
+
 
 function App() {
   return (
+    <>
     <Header/>
+    <NavegacionBar/>
+    </>
+
   );
 }
 
--- src/components/nav/navbar.jsx
+++ src/components/nav/navbar.jsx
@@ -1,33 +1,37 @@
-import React from "react";
-import { Nav, Navbar, Container, Row, Col, Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Nav, Navbar, Container, NavDropdown, } from "react-bootstrap";
+import "./navbar.css"
+import Logo from "../../assets/zonamates.png"
 
-const Navbar=() => {
-    return(
-        <>
-         <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Zona Mates</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+const NavegacionBar = () => {
+
+  
+    const [menuAbierto, setMenuAbierto] = useState(false);
+
+   
+    const abrirMenu = () => {
+      setMenuAbierto(!menuAbierto);
+    };
+  
+  return (
+    <>
+      <Container id="Navflex">
+      <button id="iconHamb" onClick={abrirMenu}>
+          <i className={`bi bi-list ${menuAbierto ? 'abierto' : ''}`} />
+        </button>
+        <div className="marca">
+          <img src={Logo} alt="logoDeZonamates" className="logozonamates"/>
+        </div>
+        
+        <div className={`Navbar ${menuAbierto ? 'abierto' : ''}`}>
+          <a href="">Inicio</a>
+          <a href="">Productos</a>
+          <a href="">Seguimiento</a>
+          <a href="">Redes</a>
+        </div>
+        
       </Container>
-    </Navbar>
-        </>
-    )
+    </>
+  )
 }
-export default Navbar;
+export default NavegacionBar
